7cdc772b30a336cce278e90ce0043b9e89cdd502
higs4281 <higs4281@gmail.com>
2022-04-29 01:40:42 -0400

    blank uris

--- marathon-cli.py
+++ marathon-cli.py
@@ -145,7 +145,6 @@ def get_marathon_json():
         "mem": 8000,
         "disk": 5000,
         "instances": 2,
-        "fetch": [],
         "uris": [],
         "user": f'{os.getenv("DOCKER_USER")}',
         "env": {
